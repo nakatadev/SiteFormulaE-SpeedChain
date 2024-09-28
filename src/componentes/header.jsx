785356cb17696aa693f6e113a104b0c70e1a2706
Rodrigo <nakataecomm@gmail.com>
2024-09-27 23:14:07 -0300

    commit inicial

---
+++ src/componentes/header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './header.css';
+import logoFormulaE from './imagens/logo Formula E - mobile.svg'
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src={logoFormulaE} alt="logo formula e" /></a>
+                    <button 
+                        className="navbar-toggler" 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#navbarNav" 
+                        aria-controls="navbarNav" 
+                        aria-expanded="false" 
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
