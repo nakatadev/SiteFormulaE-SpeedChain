88f6b87322cdf3569af70c36a18980e09faa04fe
Rodrigo <nakataecomm@gmail.com>
2024-09-29 12:03:41 -0300

    componente header concluido

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css'
 import Header from './componentes/header';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,14 +1,16 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './header.css';
-import logoFormulaE from './imagens/logo Formula E - mobile.svg'
+import logoFormulaE from './imagens/logo Formula E - mobile.svg';
+import iconeUser from './imagens/User.svg';
 
 function Header() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src={logoFormulaE} alt="logo formula e" /></a>
+                    <a className="navbar-brand" href="#"><img src={logoFormulaE} alt="logo formula e" className='logo__formulae'/></a>
                     <button 
                         className="navbar-toggler" 
                         type="button" 
@@ -26,16 +28,23 @@ function Header() {
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
+                                <a className="nav-link" href="https://www.fiaformulae.com/pt-br/news">Notícias</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
+                                <a className="nav-link" href="#">Sobre nós</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a className="nav-link" href="#">Assista</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">SpeedChain</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true"></a>
                             </li>
                         </ul>
                     </div>
+                    <a href=""><img src={iconeUser} alt="Icone de usuario" className='userIcon' /></a>
                 </div>
             </nav>
         </header>
